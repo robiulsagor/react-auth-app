b9cfc2e4771302bcf703d8159107f21cf2b760bb
robiulsagor <robiul100.me@gmail.com>
2023-10-25 21:54:18 +0600

    auth middlewares done

--- api/middlewares/authMiddleware.js
+++ api/middlewares/authMiddleware.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 
+// only admin can access
 const isAdmin = async (req, res, next) => {
     const { token } = req.cookies
 
@@ -10,10 +11,49 @@ const isAdmin = async (req, res, next) => {
     const decode = jwt.verify(token, process.env.JWT_SECRET)
 
     if (decode.role == "admin") {
+        req.user = user
         next()
     } else {
         return res.json({ msg: "You are not allowed to go!", status: "failed" })
     }
 }
 
-export { isAdmin }
+// only the current user can access
+const isCurrentUser = async (req, res, next) => {
+    const { token } = req.cookies
+    const { id } = req.params
+
+    if (!token) {
+        return res.json({ msg: "You are not authenticated!", status: "failed" })
+    }
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET)
+
+    if (decode.id === id) {
+        req.user = decode
+        next()
+    } else {
+        return res.json({ msg: "This is not your account! Delete your own account, not others'", status: "failed" })
+    }
+}
+
+// current user or the admin can access
+const isAdminOrCurrentUser = async (req, res, next) => {
+    const { token } = req.cookies
+    const { id } = req.params
+
+    if (!token) {
+        return res.json({ msg: "You are not authenticated!", status: "failed" })
+    }
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET)
+
+    if (decode.id === id || decode.role === "admin") {
+        req.user = decode
+        next()
+    } else {
+        return res.json({ msg: "This is not your account! Delete your own account, not others'", status: "failed" })
+    }
+}
+
+export { isAdmin, isCurrentUser, isAdminOrCurrentUser }
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { deleteUser, getAllUsers } from '../controllers/userController.js'
-import { isAdmin } from '../middlewares/authMiddleware.js'
+import { isAdmin, isCurrentUser } from '../middlewares/authMiddleware.js'
 const router = express.Router()
 
 router.get("/", (req, res) => {
@@ -8,6 +8,6 @@ router.get("/", (req, res) => {
 })
 
 router.get('/all', isAdmin, getAllUsers)
-router.delete('/delete/:id', deleteUser)
+router.delete('/delete/:id', isCurrentUser, deleteUser)
 
 export default router
