17eca36e6795c75f34f170c3740c62aed2749f06
robiulsagor <robiul100.me@gmail.com>
2023-10-27 00:30:58 +0600

    userSlice modification: adding Message state to Redux

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -3,7 +3,8 @@ import { createSlice } from '@reduxjs/toolkit'
 const initialState = {
     currentUser: null,
     loading: false,
-    error: false
+    error: false,
+    message: null
 }
 
 export const userSlice = createSlice({
@@ -13,22 +14,34 @@ export const userSlice = createSlice({
         signInStart: state => {
             state.loading = true;
             state.error = false;
+            state.message = null
         },
         signinSuccess: (state, action) => {
             state.currentUser = action.payload
             state.loading = false
+            state.message = null
+
         },
         signInFailed: (state, action) => {
             state.loading = false;
             state.error = action.payload;
+            state.message = null
+
         },
         signOut: state => {
             state.currentUser = null;
             state.loading = false
             state.error = false
-        }
+            state.message = null
+        },
+        setMsg: (state, action) => {
+            state.message = action.payload
+        },
+        clearMsg: (state, action) => {
+            state.message = action.payload
+        },
     }
 })
 
-export const { signInStart, signinSuccess, signInFailed, signOut } = userSlice.actions
+export const { signInStart, signinSuccess, signInFailed, signOut, setMsg, clearMsg } = userSlice.actions
 export default userSlice.reducer
